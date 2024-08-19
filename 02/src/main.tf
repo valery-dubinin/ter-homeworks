48b80ff6f48c01b249e22aceb0a6f2f10a9e6aea
valery-dubinin <valery.dubinin@gmail.com>
2024-08-19 14:31:28 +0300

    Изменения, которые будут включены в коммит: 	изменено:      main.tf 	изменено:      variables.tf К заданию 2. Переписаны значения на переменные.

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,15 +10,15 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family =  var.vm_web_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,11 +26,11 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
